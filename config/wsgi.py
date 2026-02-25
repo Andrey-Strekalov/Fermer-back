2459dfb647db7d7741c660952affb3a1a2be4b55
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-02-25 18:07:30 +0300

    раÐправка пути к насÑтройкам

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'config.settings.production'
+    'config.settings.prod'
 )
 
 from django.core.wsgi import get_wsgi_application
