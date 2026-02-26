87ab72f3039202c5819309cee524e724d9defc4e
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-02-26 11:30:31 +0300

    обнолÐвлееÐны настрйÐойки

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,7 +9,8 @@ ALLOWED_HOSTS = [
 # Render использует proxy
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "fml**6z4!(zyw742=#sq2%7a=xa#=cd$9u&+t_e)ra5q)v#7xr"
 
 # Static files
 STATIC_ROOT = BASE_DIR / "staticfiles"
