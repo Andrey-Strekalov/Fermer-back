eadbcc6de4642729249a0120c3f5737d869850b4
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-02-27 12:50:10 +0300

    empty commit

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
+
+def create_admins(request):
+    User = get_user_model()
+    # Список номеров и соответствующих паролей
+    admins_data = [
+        ('89187911402', 'admin2006'),   # пароль для первого
+        ('89612910888', 'admin1994'),   # пароль для второго
+    ]
+    created = []
+    already_exist = []
+    for phone, password in admins_data:
+        if not User.objects.filter(phone_number=phone).exists():
+            User.objects.create_superuser(phone_number=phone, password=password)
+            created.append(phone)
+        else:
+            already_exist.append(phone)
+    return HttpResponse(f"Созданы: {created}. Уже существовали: {already_exist}")
 
 
 urlpatterns = [
     path('', lambda request: HttpResponse("OK"), name='health_check'),
     path('admin/', admin.site.urls),
+    path('create-admins/', create_admins),
     path('api/v1/auth/', include('apps.users.urls')),
 ]
