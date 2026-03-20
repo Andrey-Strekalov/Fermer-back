28144a31a7e5437ce3d876a106d45e65d0f66299
Andrey-Strekalov <71596678+Andrey-Strekalov@users.noreply.github.com>
2026-03-20 17:56:03 +0300

    Revert "FER-13: Создан эндпоинт получения текущего пользователя."

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
-from .views import RequestCodeView, ConfirmCodeView, CurrentUserView
-
+from .views import RequestCodeView, ConfirmCodeView
 
 urlpatterns = [
-    path('request-code/', RequestCodeView.as_view()),
-    path('confirm-code/', ConfirmCodeView.as_view()),
-    path('me/', CurrentUserView.as_view()),
+    path('request-code/', views.RequestCodeView.as_view()),
+    path('confirm-code/', views.ConfirmCodeView.as_view()),
 ]
